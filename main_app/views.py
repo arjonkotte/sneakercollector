6c7a9e12a95a0ad5e91ab8820ebb1d0e3d9e80c9
Arjon Kotte <arjon.kotte@gmail.com>
2023-03-04 18:47:38 -0500

    upload picture functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,34 +1,67 @@
-from django.shortcuts import render
+from .models import Sneaker, Photo
+import boto3
+import uuid
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Sneaker
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'sneakercollec'
+
 # Create your views here.
 
+
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def sneakers_index(request):
-  sneakers = Sneaker.objects.all()
-  return render(request, 'sneakers/index.html', {'sneakers': sneakers})
+    sneakers = Sneaker.objects.all()
+    return render(request, 'sneakers/index.html', {'sneakers': sneakers})
 
 
 def sneaker_details(request, sneaker_id):
-  sneaker = Sneaker.objects.get(id = sneaker_id)
-  return render(request, 'sneakers/details.html', {'sneaker': sneaker})
+    sneaker = Sneaker.objects.get(id=sneaker_id)
+    return render(request, 'sneakers/details.html', {'sneaker': sneaker})
+
 
 class SneakerCreate(CreateView):
-  model = Sneaker
-  fields = '__all__'
+    model = Sneaker
+    fields = '__all__'
+
 
 class SneakerUpdate(UpdateView):
-  model = Sneaker
-  fields = '__all__'
+    model = Sneaker
+    fields = '__all__'
+
 
 class SneakerDelete(DeleteView):
-  model = Sneaker
-  success_url = '/sneakers/'
+    model = Sneaker
+    success_url = '/sneakers/'
+
+
+def add_photo(request, sneaker_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = 'sneakercollec/' + \
+            uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, sneaker_id=sneaker_id)
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('details', sneaker_id=sneaker_id)
